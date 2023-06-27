7a2360f9e5a1abe4ad89eaaf762018a5d083310b
thecookingsenpai <administration@kynesys.xyz>
2023-06-27 13:17:29 +0200

    Improved blockchain connectors

---
+++ demos/libs/classes/chainteract.js
@@ -0,0 +1,15 @@
+// INFO This module allows DEMOS to talk to different chains providing an unified API to interact with them
+
+let chains = require('./blockchains_support.js')
+
+class Chainteract {
+	constructor(chain, rpc_url, private_key=null) {
+		if (!chains[chain]) { throw new Error(`Chain ${chain} not found`) }
+        this.methods = chains[chain] // Assigning to the object just the right methods for this chain
+		this.rpc_url = rpc_url
+		this.provider = this.methods.connect(this.rpc_url, private_key)
+    }
+	// TODO Set up connections
+}
+
+module.exports = Chainteract
