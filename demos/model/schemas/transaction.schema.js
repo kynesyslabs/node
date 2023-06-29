2d29ba1baaf629f9213dc871c724578dbf48e42d
DuckDegen <duckdegen@gmail.com>
2023-06-29 19:05:16 +0200

    adding db stuff

---
+++ demos/model/schemas/transaction.schema.js
@@ -0,0 +1,32 @@
+const typeorm = require("typeorm")
+
+const TransactionSchema = new typeorm.EntitySchema({
+    name: "Transaction",
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: true,
+        },
+        content: {
+            type: "json",
+        },
+        signature: {
+            type: "varchar",
+        },
+        hash: {
+            type: "varchar",
+        },
+        confirmations: {
+            type: "int",
+        },
+        state_changes: {
+            type: "json",
+        },
+    },
+    relations: {
+        // define your relations here
+    },
+})
+
+module.exports = { TransactionSchema }
