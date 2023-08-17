143f53a36db25d7950a0a5af44ca8b55ef3a147d
megabudino <44237570+megabudino@users.noreply.github.com>
2023-08-17 11:17:32 +0200

    get web2 server and client call

---
+++ demos/src/dapps/block_explorer/src/routes/web2/client/+page.js
@@ -0,0 +1,12 @@
+import demos from '$lib/demos.js';
+export async function load ({params})
+{
+    const rpc = "http://85.208.48.187:53550";
+
+    demos.connect(rpc);
+
+    let data = demos.getWeb2Data()
+    return {
+        data: data
+    }
+}
