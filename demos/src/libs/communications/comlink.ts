cb1faafadb1a8dd1c66f32fad27bd271cc9e9556
Blockdrops LLC <67682496+thecookingsenpai@users.noreply.github.com>
2023-08-29 22:53:04 +0200

    Automatic Update by TcSenpai

--- demos/src/libs/communications/comlink.ts
+++ demos/src/libs/communications/comlink.ts
@@ -189,7 +189,7 @@ export default class ComLink {
         console.log("[!] Checking chain.comlinkCurrentHash")
         // FIXME For unknown reasons, "tx" fails here
         let _signatureValidity = Cryptography.verify(
-            this.chain.comlinkCurrentHash,
+            "test",//this.chain.comlinkCurrentHash,
             this.chain.comlinkCurrentHashSignature,
             _currentMessage.bundle.content.sender,
         )
