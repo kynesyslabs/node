1a3afd355a64fdbe885f0ade527891291ff3a144
Blockdrops LLC <67682496+thecookingsenpai@users.noreply.github.com>
2023-10-04 17:15:58 +0200

    Simplification of XMParser & updated schematics

--- demos/src/features/multichain/routines/XMParser.ts
+++ demos/src/features/multichain/routines/XMParser.ts
@@ -1,26 +1,7 @@
 // INFO In this module is offloaded the parsing of XM requests
 import * as multichain from "sdk/localsdk/multichain"
 import sendSigned from "./writes/sendSigned"
-
-// SECTION Payloads signing
-/* NOTE To better explain:
- * Once a request is received from the node, it sometimes needs data to be authenticated.
- * For example, any transaction changing status (transfers, things requiring gas, etc.) needs
- * to be properly signed. To avoid storing the private keys on the node, the node will ask
- * the client to sign locally the produced transactions. DEMOS will then be able to relay the
- * said transactions without security implications.
-*/
-export interface IXMPayload {
-    toSign: string,
-    signature: any, // REVIEW Use another type?
-    publicKey: string, // REVIEW Use another type?
-}
-
-export interface IXMPayloadRequest {
-    operationIdentifier: string, // Identifies the XMChain operation we are referring to
-    payloads: IXMPayload[], // Array of payloads to be signed for the operation
-} 
-// !SECTION Payloads signing
+import * as fs from "fs"
 
 // REVIEW Define XMScript (chs) class?
 
@@ -46,11 +27,33 @@ export interface XMScript {
 }
 
 class XMParser {
+
+    // INFO Same as below but with file support
+    static async loadFile(path: string): Promise<XMScript> {
+        if (!fs.existsSync(path)) {
+            console.log("The file does not exist.")
+            return null
+        }
+        let script = fs.readFileSync(path, "utf8")
+        return await XMParser.load(script)
+    }
 	
     // INFO Transforming a string in a XMScript
     static async load(script: string): Promise<XMScript> {
+        // Let's ensure it is already an array
+        if (!(script.startsWith("[") && script.endsWith("]"))) {
+            script = "[" + script + "]"        
+        }
         let xmscript: XMScript = JSON.parse(script)
         return xmscript
+    }
+
+    // INFO Preparsing a script to be able to execute it later (e.g. checking the syntax)
+
+    static async prepare(script: XMScript): Promise<XMScript> {
+        let result: XMScript = script
+        // TODO
+        return result
     }
 
 
@@ -78,14 +81,6 @@ class XMParser {
                     current_result = [false, "The operation requested is unknown"]
             }
             // (see FIXME above above) result.set(operation_name, current_result)
-            /* NOTE && TODO 
-             * For reasons linked to the NOTE at the beginning of this file,
-             * The reply could contain instances of the IXMPayloadRequest class.
-             * These payloads need to be acknowledged by the client (so in the client code).
-             * Please see the client implementation for more details on how this is done.
-             * Anyway, the client should be able to reply something that is in line with the
-             * IXMPayloadRequest class as described in the documentation.
-            */
             array_result.push(current_result)
         }
         return array_result
