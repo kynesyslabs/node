dd5e1af60c649c9cc7b0c14ecb4037d59a24697f
thecookingsenpai <root@vmi1322400.contaboserver.net>
2023-10-09 15:21:12 +0200

    updated update

--- demos/update.sh
+++ demos/update.sh
@@ -1,1 +1,1 @@
-git stash && cat demos_peers > our_peers && git pull && cat our_peers > demos_peers && yarn start
+cat demos_peers > our_peers && git stash && git pull && cat our_peers > demos_peers && yarn start
