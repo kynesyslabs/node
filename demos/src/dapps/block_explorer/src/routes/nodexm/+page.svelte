4641732f689cebda6e8c5217d6dfafadc663012d
megabudino <44237570+megabudino@users.noreply.github.com>
2023-10-27 15:38:15 +0200

    node xM demo

---
+++ demos/src/dapps/block_explorer/src/routes/nodexm/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { Node, Svelvet, Minimap, Controls, Drawer, ThemeToggle } from 'svelvet';
+	import OperationNode from './OperationNode.svelte';
+    import {Operation} from "$lib/chainscript.js";
+    import { v4 as uuidv4 } from 'uuid';
+	import ConditionalNode from './ConditionalNode.svelte';
+	import EqualsNode from './EqualsNode.svelte';
+
+</script>
+
+<div style="height:100dvh;">
+    <Svelvet id="my-canvas" width="100%" minimap controls theme="dark">
+        <OperationNode operation={{id:uuidv4(), label:"Pay", type:"pay", data:new Operation({tasktype:"pay"})}}/>
+        <OperationNode operation={{id:uuidv4(), label:"Pay", type:"pay", data:new Operation({tasktype:"pay"})}}/>
+        <OperationNode operation={{id:uuidv4(), label:"Read Contract", type:"contract_read", data:new Operation({tasktype:"contract_read"})}}/>
+        <OperationNode operation={{id:uuidv4(), label:"Read Contract", type:"contract_read", data:new Operation({tasktype:"contract_read"})}}/>
+        <ConditionalNode/>
+        <EqualsNode/>
+    </Svelvet>
+</div>
---
+++ demos/src/dapps/block_explorer/src/routes/nodexm/EqualsNode.svelte
@@ -0,0 +1,46 @@
+<script>
+    import {Anchor, Node} from 'svelvet';
+</script>
+<style>
+    .operation{
+        padding: 24px;
+        position: relative;
+        gap: 16px;
+        display: grid;
+        grid-template-columns: 50px 50px;
+    }
+    .operation p {
+        margin: 0;
+    }
+    .input-anchors {
+		position: absolute;
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		top: 50%;
+        transform: translate(0, -50%);
+        left: -20px;
+	}
+
+	.output-anchors {
+		position: absolute;
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+        top: 50%;
+        transform: translate(0, -50%);
+        right: -20px;
+	}
+</style>
+<Node>
+    <div class="input-anchors">
+        <Anchor input></Anchor>
+        <Anchor input></Anchor>
+    </div>
+    <div class="output-anchors">
+        <Anchor output></Anchor>
+    </div>
+    <div class="card operation">
+        <p>=</p>
+    </div>
+</Node>
