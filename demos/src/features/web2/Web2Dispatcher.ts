b0875bbab137b15582a9f7cdbbc809dacffbeb83
Blockdrops LLC <67682496+thecookingsenpai@users.noreply.github.com>
2023-11-17 15:48:13 +0100

    Fixed web2 handling & updated schematics

--- demos/src/features/web2/Web2Dispatcher.ts
+++ demos/src/features/web2/Web2Dispatcher.ts
@@ -13,7 +13,7 @@ import { deriveMempoolOperation } from "src/libs/utils/demostdlib/deriveMempoolO
 // are either first or not last of the chain), and then
 // send back to the client or to the origin rpc the
 // transaction that will be granted as web2 result
-export default async function handleWeb2(payload: IWeb2Payload, senderSocket: any): Promise<[boolean, any]> {
+export default async function handleWeb2(payload: IWeb2Payload, senderSocket: any): Promise<[boolean, Operation]> {
     // Creating the workable interface
     // TODO Remember that web2 could need to be signed and could need a fee
     // NOTE From now on, Web2API will reply to instanceName with the same instance
@@ -57,10 +57,8 @@ export default async function handleWeb2(payload: IWeb2Payload, senderSocket: an
             console.log("[web2Dispatcher] Compiling and certifying the result on our side...")
             web2interface.request.hash = hashedAttestations
             web2interface.request.signature = signedAttestations
-            // REVIEW And then we can send the response back to the client
-            console.log("[web2Dispatcher] Done! Sending the response back to the client...")
-            return [true, web2interface.request]
         } catch (error) {
+            // Catching errors before the return
             console.log("[web2Dispatcher] Error: " + error.message)
             return [false, error]
         }
@@ -75,6 +73,9 @@ export default async function handleWeb2(payload: IWeb2Payload, senderSocket: an
         // Now that our web2request.request object is updated,
         // TODO we have to merge the attestations' arrays with valid values
     }
+    // NOTE If we are here, we somehow have something to return
+    // REVIEW And then we can send the response back to the client
+    console.log("[web2Dispatcher] Done! Sending the response back to the client...")
     console.log("[web2Dispatcher] Attestations validated. Deriving a transaction...")
     let derivedTx = await toMempool(instanceName)
     console.log("[web2Dispatcher] Transaction derived.")
