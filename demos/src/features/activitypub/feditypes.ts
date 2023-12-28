267a7bc7878e94863e30054f6391c8b7e018b16c
thecookingsenpai <elmastrococinero@gmail.com>
2023-12-28 13:00:17 +0100

    Pre yarn upgrade commit

--- demos/src/features/activitypub/feditypes.ts
+++ demos/src/features/activitypub/feditypes.ts
@@ -1,50 +1,48 @@
-
-
 // Define the interfaces
 export interface ActivityPubObject {
-    "@context": string;
-    type: string;
-    id: string;
-    actor?: string;
-    object?: string;
-  }
-  
-  export interface Actor extends ActivityPubObject {
-    name: string;
-    inbox: string;
-    outbox: string;
-    followers: string;
-    following: string;
-    liked: string;
-  }
-  
-  export interface Collection {
-    "@context": string;
-    id: string;
-    type: string;
-  }
-  
-  // Function to initialize ActivityPub objects
-  export function initializeActivityPubObject(type: string): ActivityPubObject {
+    "@context": string
+    type: string
+    id: string
+    actor?: string
+    object?: string
+}
+
+export interface Actor extends ActivityPubObject {
+    name: string
+    inbox: string
+    outbox: string
+    followers: string
+    following: string
+    liked: string
+}
+
+export interface Collection {
+    "@context": string
+    id: string
+    type: string
+}
+
+// Function to initialize ActivityPub objects
+export function initializeActivityPubObject(type: string): ActivityPubObject {
     return {
-      "@context": "https://www.w3.org/ns/activitystreams",
-      type: type,
-      id: "",
+        "@context": "https://www.w3.org/ns/activitystreams",
+        type: type,
+        id: "",
     }
-  }
-  
-  // Function to initialize collections
-  export function initializeCollection(): Collection {
+}
+
+// Function to initialize collections
+export function initializeCollection(): Collection {
     return {
-      "@context": "https://www.w3.org/ns/activitystreams",
-      id: "",
-      type: "OrderedCollection",
+        "@context": "https://www.w3.org/ns/activitystreams",
+        id: "",
+        type: "OrderedCollection",
     }
-  }
-  
-  // Initialize the objects
-  export var activityPubObject = initializeActivityPubObject("")
-  export var actor: Actor = {
+}
+
+// Initialize the objects
+export var activityPubObject = initializeActivityPubObject("")
+export var actor: Actor = {
     ...initializeActivityPubObject("Person"),
     name: "",
     inbox: "",
@@ -52,15 +50,15 @@ export interface ActivityPubObject {
     followers: "",
     following: "",
     liked: "",
-  }
-  export var collection = initializeCollection()
-  export var inbox = initializeCollection()
-  export var outbox = initializeCollection()
-  export var followers = initializeCollection()
-  export var following = initializeCollection()
-  export var liked = initializeCollection()
-  export var blocked = initializeCollection()
-  export var rejections = initializeCollection()
-  export var rejected = initializeCollection()
-  export var shares = initializeCollection()
-  export var likes = initializeCollection()
+}
+export var collection = initializeCollection()
+export var inbox = initializeCollection()
+export var outbox = initializeCollection()
+export var followers = initializeCollection()
+export var following = initializeCollection()
+export var liked = initializeCollection()
+export var blocked = initializeCollection()
+export var rejections = initializeCollection()
+export var rejected = initializeCollection()
+export var shares = initializeCollection()
+export var likes = initializeCollection()
--- demos/src/features/activitypub/fediverse.ts
+++ demos/src/features/activitypub/fediverse.ts
@@ -13,61 +13,68 @@ app.use(express.json())
 
 // REVIEW Experimental universal handlers
 
-app.get("/:collection/:id", (req: { params: { collection: any; id: any; }; }, res: any) => {
-  const { collection, id } = req.params
-  console.log("Reading: " + collection + "/" + id)
-  if (!database) {
-    console.log("Database not initialized")
-    res.status(500).json({ error: "Database not initialized" })
-    return
-  }
-  // TODO Authentication
-  database.getItem(collection, id, (item: any) => {
-    if (item) {
-      res.json(item)
-    } else {
-      res.status(404).send("Not found: " + collection + "/" + id)
-    }
-  })
-})
+app.get(
+    "/:collection/:id",
+    (req: { params: { collection: any; id: any } }, res: any) => {
+        const { collection, id } = req.params
+        console.log("Reading: " + collection + "/" + id)
+        if (!database) {
+            console.log("Database not initialized")
+            res.status(500).json({ error: "Database not initialized" })
+            return
+        }
+        // TODO Authentication
+        database.getItem(collection, id, (item: any) => {
+            if (item) {
+                res.json(item)
+            } else {
+                res.status(404).send("Not found: " + collection + "/" + id)
+            }
+        })
+    },
+)
 
-app.put("/:collection/:id", (req: { params: { collection: any; id: any; }; body: any; }, res: any) => {
-  const { collection, id } = req.params
-  console.log("Updating: " + collection + "/" + id)
-  if (!database) {
-    console.log("Database not initialized")
-    res.status(500).json({ error: "Database not initialized" })
-    return
-  }
-  // TODO Authentication
-  database.saveItem(collection, req.body)
-  res.json(req.body)
-})
-
+app.put(
+    "/:collection/:id",
+    (req: { params: { collection: any; id: any }; body: any }, res: any) => {
+        const { collection, id } = req.params
+        console.log("Updating: " + collection + "/" + id)
+        if (!database) {
+            console.log("Database not initialized")
+            res.status(500).json({ error: "Database not initialized" })
+            return
+        }
+        // TODO Authentication
+        database.saveItem(collection, req.body)
+        res.json(req.body)
+    },
+)
 
 async function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 // NOTE INFO ANCHOR Starting our main function without blocking
 async function main() {
-  let counter = 0
-  while (!connected) {
-    await sleep(1000)
-    counter++
-    if (counter > 10) {
-      console.log("Timeout: server never came alive")
-      process.exit(1)
+    let counter = 0
+    while (!connected) {
+        await sleep(1000)
+        counter++
+        if (counter > 10) {
+            console.log("Timeout: server never came alive")
+            process.exit(1)
+        }
     }
-  }
 
-  // Creating or opening a database connection
-  database = new ActivityPubStorage("./db.sqlite3")
-  console.log("Connected to database")
-
+    // Creating or opening a database connection
+    database = new ActivityPubStorage("./db.sqlite3")
+    console.log("Connected to database")
 }
 main()
 
 // Start the server
 const port = process.env.PORT || 3000
-app.listen(port, () => {console.log(`ActivityPub server listening on port ${port}`); connected = true })
+app.listen(port, () => {
+    console.log(`ActivityPub server listening on port ${port}`)
+    connected = true
+})
