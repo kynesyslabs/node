6225fe1bc329dcfbee295a5a6d8dbbf630c2fe97
thecookingsenpai <elmastrococinero@gmail.com>
2024-01-22 21:00:52 +0100

    wssed

--- sdk/localsdk/multichain/configs/chainProviders.ts
+++ sdk/localsdk/multichain/configs/chainProviders.ts
@@ -1,6 +1,6 @@
 export const chainProviders = {
 	"ripple": {
 		"mainnet": "https://s1.ripple.com:51234/",
-        "testnet": "https://s.altnet.rippletest.net:51234/",
+        "testnet": "wss://s.altnet.rippletest.net:51233/",
 	},
 }
