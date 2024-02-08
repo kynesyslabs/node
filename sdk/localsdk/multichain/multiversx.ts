3e68af8729e4502579fa7ba79c0ba5d6ae1d30a3
mungai-njoroge <geoffreymungai45@gmail.com>
2024-02-08 13:33:53 +0300

    set up multiversx pay method
    + handle Mnemonic keyFile in connectWallet()

--- sdk/localsdk/multichain/multiversx.ts
+++ sdk/localsdk/multichain/multiversx.ts
@@ -8,8 +8,13 @@ Human readable license: https://creativecommons.org/licenses/by-nc-nd/4.0/
 KyneSys Labs: https://www.kynesys.xyz/
 
 */
-
-import { Mnemonic, UserWallet } from "@multiversx/sdk-wallet"
+import {
+    Account,
+    Transaction,
+    TokenTransfer,
+    TransactionPayload,
+} from "@multiversx/sdk-core"
+import { Mnemonic, UserWallet, UserWalletKind } from "@multiversx/sdk-wallet"
 import { ProxyNetworkProvider } from "@multiversx/sdk-network-providers"
 
 import { UserAddress } from "@multiversx/sdk-wallet/out/userAddress"
@@ -22,6 +27,10 @@ export default class MULTIVERSX extends DefaultChainAsync {
     declare provider: INetworkProvider
     declare wallet: UserWallet
 
+    // TODO: Review the use of #private properties
+    #chainID: string
+    #walletPubKeybech32: string
+
     constructor(rpcURL: string) {
         super(rpcURL)
         this.name = "multiversx"
@@ -33,7 +42,11 @@ export default class MULTIVERSX extends DefaultChainAsync {
         this.provider = new ProxyNetworkProvider(this.rpc_url)
 
         const networkConfig = await this.provider.getNetworkConfig()
-        this.connected = networkConfig.ChainID !== undefined
+        const chainID = networkConfig.ChainID
+
+        // NOTE: Chain ID is needed in this.pay()
+        this.connected = chainID !== undefined
+        this.#chainID = chainID
 
         return this.connected
     }
@@ -54,8 +67,6 @@ export default class MULTIVERSX extends DefaultChainAsync {
         const secretKey = mnemonics.deriveKey(addressIndex, password)
         const wallet = UserWallet.fromSecretKey({ secretKey, password })
 
-        console.log(wallet.toJSON())
-
         const jsonWallet = wallet.toJSON()
 
         // NOTE: .bech32 is the address property
@@ -71,16 +82,39 @@ export default class MULTIVERSX extends DefaultChainAsync {
     }
 
     connectWallet(privateKey: string, password: string) {
-        // NOTE: privateKey is the keyfile in a JSON string format
-        // NOTE: the password param is not defined in DefaultChainAsync
+        // NOTE: privateKey is the keyFile in a JSON string format
+        // NOTE: the password param is not yet defined in DefaultChainAsync
 
-        required(privateKey, "Key file is required to connect to the wallet.")
+        required(privateKey, "KeyFile is required to connect to the wallet.")
         required(password, "Password is required to decrypt the key file.")
 
         const keyfile = JSON.parse(privateKey)
 
-        const secretKey = UserWallet.decryptSecretKey(keyfile, password)
-        this.wallet = UserWallet.fromSecretKey({ secretKey, password })
+        switch (keyfile.kind) {
+            case UserWalletKind.Mnemonic:
+                const mnemonic = UserWallet.decryptMnemonic(keyfile, password)
+                this.wallet = UserWallet.fromMnemonic({
+                    mnemonic: mnemonic.toString(),
+                    password,
+                })
+
+                const key = mnemonic.deriveKey()
+                this.#walletPubKeybech32 = key
+                    .generatePublicKey()
+                    .toAddress()
+                    .bech32()
+                break
+
+            case UserWalletKind.SecretKey:
+                const secretKey = UserWallet.decryptSecretKey(keyfile, password)
+                this.wallet = UserWallet.fromSecretKey({ secretKey, password })
+                this.#walletPubKeybech32 = this.wallet.toJSON().bech32
+
+                break
+
+            default:
+                throw new Error("MULTIVERSX: Invalid KeyFile")
+        }
 
         return this.wallet
     }
@@ -94,9 +128,38 @@ export default class MULTIVERSX extends DefaultChainAsync {
         return account.balance.toString()
     }
 
-    pay(receiver: string, amount: string): Promise<any> {
-        throw new Error("Method not implemented.")
+    async pay(receiver: string, amount: string): Promise<Transaction> {
+        // NOTE: provider is required here because we need the Chain ID which is resolved in this.connect()
+
+        required(this.provider, "Provider not connected")
+        required(this.#walletPubKeybech32, "Wallet is not connected")
+
+        const senderAdress = UserAddress.fromBech32(this.#walletPubKeybech32)
+
+        // INFO: Sync sender account which is needed to get a nonce
+        const senderAccount = new Account(senderAdress)
+        const senderOnNetwork = await this.provider.getAccount(senderAdress)
+        senderAccount.update(senderOnNetwork)
+
+        const receiverAdress = UserAddress.fromBech32(receiver)
+
+        const transfer = TokenTransfer.egldFromAmount(amount)
+
+        const tx = new Transaction({
+            // data: new TransactionPayload(""),
+            gasLimit: 80000,
+            sender: senderAdress,
+            receiver: receiverAdress,
+            value: transfer,
+            chainID: this.#chainID,
+        })
+
+        tx.setNonce(senderAccount.getNonceThenIncrement())
+
+        // INFO: Return for signing and broadcast
+        return tx
     }
+
     info(): Promise<string> {
         throw new Error("Method not implemented.")
     }
--- sdk/localsdk/multichain/multiversx/test.ts
+++ sdk/localsdk/multichain/multiversx/test.ts
@@ -1,34 +1,54 @@
-import termkit from "terminal-kit"
+import path from "path"
+import fs from "node:fs/promises"
+import { fileURLToPath } from "url"
 
 import MULTIVERSX from "../multiversx"
 
-const term = termkit.terminal
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const TESTNET_URL = "https://testnet-api.multiversx.com"
 const VALID_TESTNET_ADDRESS =
     "erd1fsac7hpfzyhzs2ls894579kctfzp8n3hyp6gt5n0ccnd6hp9dpkqd6hg6w"
+
+async function readKeyfileFromPath(_path: string) {
+    const fullpath = path.join(__dirname, _path)
+
+    // INFO: Read the keyfile from the path
+    const file = await fs.readFile(fullpath, {
+        encoding: "utf-8",
+    })
+
+    return file.trim()
+}
 
 export default async function testMultiversx() {
     const WALLET_PASSWORD = "password"
 
     const multiversx = new MULTIVERSX(TESTNET_URL)
 
-    // Connecting to the network
+    // INFO: Connecting to the network
     console.log("starting connection")
     await multiversx.connect()
 
-    // Generating a wallet
+    // INFO: Generating a wallet
     const { wallet_keyfile, address } = multiversx.createWallet(WALLET_PASSWORD)
+    console.log("[MULTIVERSX] GENERATED KEY FILE")
+    console.log(wallet_keyfile)
 
-    // Connecting to the wallet
-    multiversx.connectWallet(wallet_keyfile, WALLET_PASSWORD)
-    console.log(multiversx.wallet)
+    // INFO: Connecting to the wallet
+    const VALID_KEYFILE = await readKeyfileFromPath("keyfile.json")
+    const VALID_KEYFILE_PASSWORD = await readKeyfileFromPath(
+        "keyfile_password.txt",
+    )
 
-    // Checking if the connection points to the same wallet
-    const is_same_wallet = multiversx.wallet.toJSON().bech32 === address
+    multiversx.connectWallet(VALID_KEYFILE, VALID_KEYFILE_PASSWORD)
 
-    term.bgCyan("Wallet created and connected: " + is_same_wallet + "\n")
-
-    // Getting the balance
+    // INFO: Getting the balance
     const balance = await multiversx.getBalance(VALID_TESTNET_ADDRESS)
     console.log("Balance: " + balance)
+
+    // INFO: ESDT Transfer
+    const tx = await multiversx.pay(address, "1.5")
+    console.log(tx)
 }
