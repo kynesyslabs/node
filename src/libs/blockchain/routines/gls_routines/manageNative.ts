7a3c18f11ada0e7c235b01839b5cd97eb6c98eb3
tcsenpai <thecookingsenpai@countermail.com>
2024-03-01 22:35:59 +0100

    implemented balance methods

--- src/libs/blockchain/routines/gls_routines/manageNative.ts
+++ src/libs/blockchain/routines/gls_routines/manageNative.ts
@@ -10,17 +10,21 @@ import { StatusNative } from "src/model/entities/StatusNative"
 
 // SECTION Balance management
 
-// INFO Transfer a balance from one user to another
-async function transferBalance() {}
-
-// INFO Add a balance to a user
-async function addBalance() {}
-
-// INFO Remove a balance from a user
-async function removeBalance() {}
+// INFO Get the balance of a user
+async function balance(address: string): Promise<number> {
+    const db = await Datasource.getInstance()
+    const StatusNativeRepository = db
+        .getDataSource()
+        .getRepository(StatusNative)
+    const status = await StatusNativeRepository.findOneBy({ address })
+    return status.balance
+}
 
 // INFO Arbitrary function to set the balance of a user
-async function setBalance(address: string, balance: number) {
+async function setBalance(
+    address: string,
+    balance: number,
+): Promise<[boolean, string]> {
     const rawData = {
         address: address,
         balance: balance,
@@ -32,11 +36,57 @@ async function setBalance(address: string, balance: number) {
     const StatusNativeRepository = db
         .getDataSource()
         .getRepository(StatusNative)
-    return await StatusNativeRepository.save(rawData)
+    await StatusNativeRepository.save(rawData)
+    return [true, ""]
 }
 
-// INFO Get the balance of a user
-async function balance() {}
+// INFO Add a balance to a user
+async function addBalance(
+    address: string,
+    amount: number,
+): Promise<[boolean, string]> {
+    // Get the current balance
+    const currentBalance = await balance(address)
+    // Add the amount
+    const newBalance = currentBalance + amount
+    // Set the new balance
+    await setBalance(address, newBalance)
+    return [true, ""]
+}
+
+// INFO Remove a balance from a user
+async function removeBalance(
+    address: string,
+    amount: number,
+): Promise<[boolean, string]> {
+    // Get the current balance
+    const currentBalance = await balance(address)
+    // NOTE Check if the user has enough balance
+    if (currentBalance < amount) {
+        return [false, "Insufficient balance"]
+    }
+    // Remove the amount
+    const newBalance = currentBalance - amount
+    // Set the new balance
+    await setBalance(address, newBalance)
+    return [true, ""]
+}
+
+// INFO Transfer a balance from one user to another
+async function transferBalance(
+    address_from: string,
+    address_to: string,
+    amount: number,
+): Promise<[boolean, string]> {
+    // Remove the amount from the sender
+    let success = await removeBalance(address_from, amount)
+    if (!success[0]) {
+        return [false, success[1]]
+    }
+    // Add the amount to the receiver
+    await addBalance(address_to, amount)
+    return [true, ""]
+}
 
 // !SECTION balance management
 
