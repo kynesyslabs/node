2b404929275853bc746aa9d16911e1424560483a
tcsenpai <thecookingsenpai@countermail.com>
2024-03-12 19:42:08 +0100

    Cleanup + dockerfile

--- src/features/experimental/fhe/FHE.ts
+++ src/features/experimental/fhe/FHE.ts
@@ -9,6 +9,10 @@ import { BatchEncoder } from "node-seal/implementation/batch-encoder"
 import { Encryptor } from "node-seal/implementation/encryptor"
 import { Decryptor } from "node-seal/implementation/decryptor"
 import { Evaluator } from "node-seal/implementation/evaluator"
+import { CipherText } from "node-seal/implementation/cipher-text"
+
+import Hashing from "src/libs/crypto/hashing"
+import { Hash } from "crypto"
 
 export default class FHE {
     public static instance: FHE
@@ -31,6 +35,13 @@ export default class FHE {
     public encryptor: Encryptor
     public decryptor: Decryptor
     public evaluator: Evaluator
+
+    private internalValues = {
+        zero: {
+            plain: 0,
+            cipher: null,
+        },
+    }
 
     constructor() {}
 
@@ -58,105 +69,123 @@ export default class FHE {
         this.parms = this.seal.EncryptionParameters(this.schemeType)
     }
 
-    // NOTE Set the parameters and create the context
-    public async setParameters(): Promise<void> {
-        // Set the PolyModulusDegree
-        this.parms.setPolyModulusDegree(this.polyModulusDegree)
+    public config = {
+        // NOTE Set the parameters and create the context
+        setParameters: async (): Promise<void> => {
+            // Set the PolyModulusDegree
+            this.parms.setPolyModulusDegree(this.polyModulusDegree)
 
-        // Create a suitable set of CoeffModulus primes
-        this.parms.setCoeffModulus(
-            this.seal.CoeffModulus.Create(
-                this.polyModulusDegree,
-                Int32Array.from(this.bitSizes),
-            ),
-        )
-
-        // Set the PlainModulus to a prime of bitSize 20.
-        this.parms.setPlainModulus(
-            this.seal.PlainModulus.Batching(
-                this.polyModulusDegree,
-                this.bitSize,
-            ),
-        )
-
-        // Create the context
-        this.context = this.seal.Context(
-            this.parms, // Encryption Parameters
-            true, // ExpandModChain
-            this.securityLevel, // Enforce a security level
-        )
-
-        if (!this.context.parametersSet()) {
-            throw new Error(
-                "Could not set the parameters in the given context. Please try different encryption parameters.",
+            // Create a suitable set of CoeffModulus primes
+            this.parms.setCoeffModulus(
+                this.seal.CoeffModulus.Create(
+                    this.polyModulusDegree,
+                    Int32Array.from(this.bitSizes),
+                ),
             )
-        }
+
+            // Set the PlainModulus to a prime of bitSize 20.
+            this.parms.setPlainModulus(
+                this.seal.PlainModulus.Batching(
+                    this.polyModulusDegree,
+                    this.bitSize,
+                ),
+            )
+
+            // Create the context
+            this.context = this.seal.Context(
+                this.parms, // Encryption Parameters
+                true, // ExpandModChain
+                this.securityLevel, // Enforce a security level
+            )
+
+            if (!this.context.parametersSet()) {
+                throw new Error(
+                    "Could not set the parameters in the given context. Please try different encryption parameters.",
+                )
+            }
+        },
+
+        // NOTE Create the keys and the encoders
+        createKeysAndEncoders: async (): Promise<void> => {
+            // Create the keys
+            this.keyGenerator = this.seal.KeyGenerator(this.context)
+            this.publicKey = this.keyGenerator.createPublicKey()
+            this.secretKey = this.keyGenerator.secretKey()
+            this.encoder = this.seal.BatchEncoder(this.context)
+            this.encryptor = this.seal.Encryptor(this.context, this.publicKey)
+            this.decryptor = this.seal.Decryptor(this.context, this.secretKey)
+            this.evaluator = this.seal.Evaluator(this.context)
+
+            // We can now encrypt some standard values to be used later
+            this.internalValues.zero.cipher =
+                await this.encryption.encryptNumber(
+                    this.internalValues.zero.plain,
+                )
+        },
     }
 
-    // NOTE Create the keys and the encoders
-    public async createKeysAndEncoders(): Promise<void> {
-        // Create the keys
-        this.keyGenerator = this.seal.KeyGenerator(this.context)
-        this.publicKey = this.keyGenerator.createPublicKey()
-        this.secretKey = this.keyGenerator.secretKey()
-        this.encoder = this.seal.BatchEncoder(this.context)
-        this.encryptor = this.seal.Encryptor(this.context, this.publicKey)
-        this.decryptor = this.seal.Decryptor(this.context, this.secretKey)
-        this.evaluator = this.seal.Evaluator(this.context)
+    public encryption = {
+        // NOTE Encrypt a number
+        encryptNumber: async (num: number): Promise<void | CipherText> => {
+            // Encode the Array
+            const plainText = this.encoder.encode(Int32Array.from([num]))
+
+            // Encrypt the PlainText
+            if (plainText) {
+                const cipherText = this.encryptor.encrypt(plainText)
+                return cipherText
+            }
+
+            return
+        },
+
+        // NOTE Decrypt a number
+        decryptNumber: async (cipherText: any): Promise<number> => {
+            // Decrypt the CipherText
+            const decryptedPlainText = this.decryptor.decrypt(cipherText)
+
+            // Decode the PlainText
+            if (decryptedPlainText) {
+                const decodedArray = this.encoder.decode(decryptedPlainText)
+                return decodedArray[0]
+            }
+
+            return
+        },
     }
 
-    // NOTE Encrypt a number
-    public async encryptNumber(num: number): Promise<any> {
-        // Encode the Array
-        const plainText = this.encoder.encode(Int32Array.from([num]))
+    public math = {
+        // NOTE Add two numbers
+        addNumbers: async (
+            cipherText1: any,
+            cipherText2: any,
+        ): Promise<any> => {
+            // Add the CipherText to itself and store it in the destination parameter (itself)
+            this.evaluator.add(cipherText1, cipherText2, cipherText1) // Op (A), Op (B), Op (Dest)
 
-        // Encrypt the PlainText
-        if (plainText) {
-            const cipherText = this.encryptor.encrypt(plainText)
+            return cipherText1
+        },
+
+        // NOTE Multiply two numbers
+        multiplyNumbers: async (
+            cipherText1: any,
+            cipherText2: any,
+        ): Promise<any> => {
+            // Multiply the CipherText to itself and store it in the destination parameter (itself)
+            this.evaluator.multiply(cipherText1, cipherText2, cipherText1) // Op (A), Op (B), Op (Dest)
+
+            return cipherText1
+        },
+
+        // NOTE Flip operation
+        negate: async (cipherText: any): Promise<any> => {
+            this.evaluator.negate(cipherText, cipherText) // Op (A), Op (Dest)
             return cipherText
-        }
-
-        return
+        },
     }
 
-    // NOTE Decrypt a number
-    public async decryptNumber(cipherText: any): Promise<number> {
-        // Decrypt the CipherText
-        const decryptedPlainText = this.decryptor.decrypt(cipherText)
-
-        // Decode the PlainText
-        if (decryptedPlainText) {
-            const decodedArray = this.encoder.decode(decryptedPlainText)
-            return decodedArray[0]
-        }
-
-        return
-    }
-
-    // NOTE Add two numbers
-    public async addNumbers(cipherText1: any, cipherText2: any): Promise<any> {
-        // Add the CipherText to itself and store it in the destination parameter (itself)
-        this.evaluator.add(cipherText1, cipherText2, cipherText1) // Op (A), Op (B), Op (Dest)
-
-        return cipherText1
-    }
-
-    // NOTE Multiply two numbers
-    public async multiplyNumbers(
-        cipherText1: any,
-        cipherText2: any,
-    ): Promise<any> {
-        // Multiply the CipherText to itself and store it in the destination parameter (itself)
-        this.evaluator.multiply(cipherText1, cipherText2, cipherText1) // Op (A), Op (B), Op (Dest)
-
-        return cipherText1
-    }
-
-    // NOTE Boolean operations
-    public async negate(cipherText1: any, cipherText2: any): Promise<any> {
-        let res = this.evaluator.negate(cipherText1, cipherText2) // Op (A), Op (Dest)
-        return res
-    }
+    // NOTE Experimental boolean operations
+    public booleans = {}
 
     // NOTE Fallback to plain call
     public async call(methodName: string, [cipherText1, cipherText2]: any[]) {
@@ -168,35 +197,3 @@ export default class FHE {
         }
     }
 }
-
-/*
-
-async function main() {
-    const fhe = await FHE.getInstance()
-    await fhe.setParameters()
-    await fhe.createKeysAndEncoders()
-
-    // Create data to be encrypted
-    const array = Int32Array.from([1, 2, 3, 4, 5])
-
-    // Encrypt the PlainText
-    var cipherFirstNumber = await fhe.encryptNumber(1)
-    var cipherSecondNumber = await fhe.encryptNumber(2)
-    // Add the CipherText to itself and store it in the destination parameter (itself)
-    var cipherText_added = await fhe.addNumbers(cipherFirstNumber, cipherSecondNumber)
-    // Decrypt the CipherText
-    var decryptedPlainText = await fhe.decryptNumber(cipherText_added)
-    console.log("decryptedPlainText", decryptedPlainText)
-
-    // Encrypt the PlainText for the first number which was modified before
-    var cipherFirstNumber = await fhe.encryptNumber(1)
-    // Multiply the CipherText to itself and store it in the destination parameter (itself)
-    var cipherText_multiplied = await fhe.multiplyNumbers(cipherFirstNumber, cipherSecondNumber)
-    // Decrypt the CipherText
-    var decryptedPlainText = await fhe.decryptNumber(cipherText_multiplied)
-    console.log("decryptedPlainText", decryptedPlainText)
-}
-
-main()
-
-*/
