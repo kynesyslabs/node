e6351e7e8d8ff956837c8277db4ba9a3c42182aa
mungai-njoroge <geoffreymungai45@gmail.com>
2024-04-18 17:20:01 +0300

    remove dead type files
    + remove xm sdks and their dependencies
    + remove tests and github workflow

--- src/libs/blockchain/types/addressInfo.ts
+++
@@ -1,8 +0,0 @@
-// import { StatusNative } from "src/model/entities/StatusNative"
-// import { StatusProperties } from "src/model/entities/StatusProperties"
-// import { StatusNative, StatusProperties } from "@kynesyslabs/demosdk/types"
-
-// export default interface AddressInfo {
-//     native: StatusNative | null
-//     properties: StatusProperties | null
-// }
--- src/libs/blockchain/types/genesisTypes.ts
+++ src/libs/blockchain/types/genesisTypes.ts
@@ -11,48 +11,6 @@ import Chain from "../chain"
  *
  */
 
-// SECTION Primitives
-
-// export interface GenesisImmutableProperties {
-//     id: number
-//     name: string
-//     currency: string
-// }
-
-// export interface GenesisMutableProperties {
-//     minBlocksForValidationOnlineStatus: number
-// }
-
-// export interface GenesisArtifact {
-//     properties: GenesisImmutableProperties
-//     mutables: GenesisMutableProperties
-//     balances: [[address: string, amount: string]]
-//     timestamp: number
-//     previous_genesis_hash: string
-//     previous_block_hash: string
-//     signature: string
-//     hash: string
-//     number: number
-// }
-
-// // !SECTION Primitives
-
-// // SECTION Components
-
-// export interface StandardGenesis {
-//     properties: GenesisImmutableProperties
-//     mutables: GenesisMutableProperties
-//     balances: [[address: string, amount: string]]
-//     timestamp: number
-// }
-
-// export interface forkGenesis extends StandardGenesis {
-//     previous_genesis_hash: string
-//     previous_block_hash: string
-// }
-
-// !SECTION Components
-
 // INFO The Genesis class with its methods
 export default class Genesis {
     genesisBlock: Blocks //  The genesis block
