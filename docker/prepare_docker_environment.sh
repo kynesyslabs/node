b35570400eee25cbfc7f854916bcc3d58aa5426c
tcsenpai <thecookingsenpai@countermail.com>
2024-05-04 18:35:34 +0200

    simplified and made lightweight the docker replication

--- docker/prepare_docker_environment.sh
+++ docker/prepare_docker_environment.sh
@@ -3,13 +3,8 @@
 # This is done to always have a copy of the repository in the docker folder, so that the docker image is able to work
 # on its own.
 cd ..
-mkdir -p docker/node_1
+mkdir -p docker/node_base
 rsync -av --exclude=node_modules --exclude=docker --exclude=.trunk --exclude=.vscode \
           --exclude=demos_peers --exclude=.demos_identity --exclude=/data/chain.db --exclude=pubkey \
           --exclude=.git \
-           ./ ./docker/node_1
-mkdir -p docker/node_2
-rsync -av --exclude=node_modules --exclude=docker --exclude=.trunk --exclude=.vscode \
-          --exclude=demos_peers --exclude=.demos_identity --exclude=/data/chain.db --exclude=pubkey \
-          --exclude=.git \
-           ./ ./docker/node_2
+           ./ ./docker/node_base
