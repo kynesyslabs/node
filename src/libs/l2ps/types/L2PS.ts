6f6b490e312095e298a461685d6b9a6882fcc0e7
tcsenpai <thecookingsenpai@countermail.com>
2024-06-05 02:50:42 +0200

    refactor: Remove unused import and progressed encryption/decryption methods in L2PS class

--- src/libs/l2ps/types/L2PS.ts
+++ src/libs/l2ps/types/L2PS.ts
@@ -8,15 +8,18 @@
     Moreover, they must be able to autonomously encrypt and decrypt messages on the L2PS they
     are part of.
 */
-import IPartecipant from "./Partecipants"
 import Hashing from "src/libs/crypto/hashing"
 import Cryptography from "src/libs/crypto/cryptography"
 import * as forge from "node-forge"
 import required from "src/utilities/required"
+import { decrypt } from "dotenv"
+import { Transaction } from "@kynesyslabs/demosdk/types"
+
+/* TODOs and ideas
+    * Should we have a mechanism for key rotation? // TODO
+*/
 
 export default class L2PS {
-
-    partecipants: IPartecipant[] = []
 
     // Compiled at construction time based on the input
     rsaKeyPair: forge.pki.rsa.KeyPair = null
@@ -40,7 +43,7 @@ export default class L2PS {
                 this.uid = _uid
             }
             // We need an existing L2PS to fetch the partecipants list, as it is not created from scratch
-            let existing = this.fetchFromGLS(Hashing.sha256(_rsaKeyPair.publicKey.n.toString(16)))
+            let existing = this.fetchL2PSFromGLS(Hashing.sha256(_rsaKeyPair.publicKey.n.toString(16)))
             // If the L2PS does not exist, we throw an error
             if (!existing) {
                 throw new Error("L2PS not found")
@@ -54,28 +57,52 @@ export default class L2PS {
         }
     }
 
+    // Encrypt the message for the L2PS
+    // NOTE This can be used by anyone, not only partecipants, as it requires the public key
     public encryptForL2PS(message: string): string {
-        // TODO Encrypt the message for the L2PS
-        return ""
+        let encrypted = this.rsaKeyPair.publicKey.encrypt(forge.util.encode64(message))
+        return encrypted
     }
 
+    // Decrypt the message from the L2PS
+    // NOTE This can be used only by partecipants as it requires the private key
     public decryptFromL2PS(message: string): string {
-        // TODO Decrypt the message from the L2PS
-        return ""
+        required(this.partecipant, "partecipant pre-flight check failed")
+        required(this.rsaKeyPair.privateKey, "privateKey is missing")
+        let decrypted = this.rsaKeyPair.privateKey.decrypt(message)
+        return decrypted
     }
 
     // SECTION Class methods
 
+    // * Management methods
+
     // Fetch the L2PS info from the GLS
-    public async fetchFromGLS(uid: string): Promise<void> {
+    public async fetchL2PSFromGLS(uid: string): Promise<void> {
         // TODO Call the GLS to see if we have a L2PS with this UID
         // TODO If we have it, fetch the partecipants list and fill the object
         // TODO We also fill the uid, while the keypair can be left untouched (non partecipant import)
     }
 
-    // Check if a partecipant is part of the L2PS
-    public isPartecipant(partecipant: IPartecipant): boolean {
-        return this.partecipants.find(p => p.id === partecipant.id) !== undefined
+    // * Tx methods
+
+    // Fetch and decrypt a Tx from the L2PS registry in the GLS
+    public async fetchTxFromGLS(txId: string): Promise<Transaction> {
+        required(this.partecipant, "partecipant pre-flight check failed")
+        required(this.rsaKeyPair.privateKey, "privateKey is missing")
+        // TODO Call the GLS to fetch a Tx from the L2PS registry in the GLS
+        // TODO The Tx must be decrypted with the private key of the L2PS
+        // TODO The Tx must contain the UID of the L2PS (create a proper type for this)
+        // TODO Return the decrypted Tx
+        return null
+    }
+
+    // Insert an encrypted Tx into the L2PS registry in the GLS
+    public async registerTxToGLS(tx: Transaction): Promise<void> {
+        let encryptedTx = this.encryptForL2PS(JSON.stringify(tx))
+        // TODO Call the GLS to register a Tx into the L2PS registry in the GLS
+        // TODO The Tx must be encrypted with the public key of the L2PS
+        // TODO The Tx must contain the UID of the L2PS (create a proper type for this)
     }
 
 
