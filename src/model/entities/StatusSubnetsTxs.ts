c6ac52eedd8690ad24eaf4f70fec685c1d155ac5
tcsenpai <dev@tcsenpai.com>
2024-10-24 22:48:38 +0200

    added a table for subnets txs

---
+++ src/model/entities/StatusSubnetsTxs.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryColumn } from "typeorm"
+import { EncryptedTransaction } from "@kynesyslabs/demosdk/types"
+/* INFO
+    Subnet transactions (l2ps) are stored in a native table so they are synced with the rest of the chain.
+    The transactions are indexed by the tx hash, the subnet id, the status and the block hash and number.
+    This allows for a quick lookup to the transaction details and the block they were included in.
+*/
+
+@Entity("status_subnets_txs")
+export class StatusSubnetsTxs {
+    @PrimaryColumn("text", { name: "tx_hash" })
+    tx_hash: string
+
+    @Column("text", { name: "subnet_id" })
+    subnet_id: string
+
+    @Column("text", { name: "status" })
+    status: string
+
+    @Column("text", { name: "block_hash" })
+    block_hash: string
+
+    @Column("integer", { name: "block_number" })
+    block_number: number
+
+    @Column("json", { name: "tx_data"})
+    tx_data: EncryptedTransaction
+}
