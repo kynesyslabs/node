3bbe48def5aba91edcf8a9f485f1a6a50ccf014d
tcsenpai <dev@tcsenpai.com>
2024-11-01 20:11:12 +0100

    typo in folder name

--- src/libs/blockchain/routines/subOperations.ts
+++ src/libs/blockchain/routines/subOperations.ts
@@ -9,7 +9,7 @@ import GCR from "../gcr/gcr"
 import Genesis from "../types/genesisTypes"
 // NOTE Due to the modularity of the code, many routines will be stored in their own modules
 // TODO Move everything there if possible
-import gcrRoutines from "../gcr/grc_routines"
+import gcrRoutines from "../gcr/gcr_routines"
 
 // REVIEW Is this working?
 export default class subOperations {
