fa087af528f2ede5a7a67d2a8ed32e5c1b54e966
cwilvx <geoffreymungai45@gmail.com>
2024-11-07 15:42:01 +0300

    log validator status at ensureCandidateBlockFormed

--- src/libs/consensus/v2/routines/ensureCandidateBlockFormed.ts
+++ src/libs/consensus/v2/routines/ensureCandidateBlockFormed.ts
@@ -1,20 +1,33 @@
 import { getSharedState } from "src/utilities/sharedState"
 import { consensusRoutine } from "../PoRBFT"
 import log from "src/utilities/logger"
+import { getShardManager } from "./shardManager"
 
 export default async function ensureCandidateBlockFormed(): Promise<boolean> {
-    
     let success = false
     if (!getSharedState.candidateBlock) {
-        log.info("Candidate block not formed yet, forcing the consensus routine...")
+        log.info(
+            "Candidate block not formed yet, forcing the consensus routine...",
+        )
         if (!getSharedState.inConsensusLoop) {
             await consensusRoutine()
         } else {
-            log.info("Consensus routine already running, waiting for it to finish...")
+            log.info(
+                "Consensus routine already running, waiting for it to finish...",
+            )
         }
     }
+
+    const ourValidatorStatus = getShardManager.shardStatus.get(
+        getSharedState.identity.ed25519.publicKey.toString("hex"),
+    )
+    log.info(
+        "Our validator status: " + JSON.stringify(ourValidatorStatus, null, 2),
+    )
+
     if (getSharedState.candidateBlock) {
         success = true
     }
+
     return success
 }
