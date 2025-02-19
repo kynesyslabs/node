818cab385a3d77f64d4e19cbbddfc55283aba445
cwilvx <geoffreymungai45@gmail.com>
2025-02-19 14:02:21 +0300

    fix identities

--- src/libs/blockchain/gcr/gcr_routines/ensureGCRForUser.ts
+++ src/libs/blockchain/gcr/gcr_routines/ensureGCRForUser.ts
@@ -3,20 +3,18 @@
 import Datasource from "src/model/datasource"
 import { GlobalChangeRegistry } from "src/model/entities/GCR/GlobalChangeRegistry"
 import HandleGCR from "../handleGCR"
-export default async function ensureGCRForUser(
-    publicKey: string,
-): Promise<any> {
+import { GCR_Main } from "@/model/entities/GCRv2/GCR_Main"
+export default async function ensureGCRForUser(pubkey: string): Promise<GCR_Main> {
     // TODO Create the GCR for the user if it doesn't exist
     // TODO Create the GCR extended for the user if it doesn't exist
     const db = await Datasource.getInstance()
-    const GCRRepository = db.getDataSource().getRepository(GlobalChangeRegistry)
+    const GCRRepository = db.getDataSource().getRepository(GCR_Main)
 
-    let gcr = await GCRRepository.findOne({ where: { publicKey } })
+    let gcr = await GCRRepository.findOne({ where: { pubkey } })
 
     if (!gcr) {
         // Create the GCR for the user
-        await HandleGCR.createAccount(publicKey)
-        gcr = await GCRRepository.findOne({ where: { publicKey } })
+        return await HandleGCR.createAccount(pubkey)
     }
 
     return gcr
