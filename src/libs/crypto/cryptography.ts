9b4fb751ce06415d56ff24a903ef72d76cd45935
tcsenpai <tcsenpai@discus.sh>
2025-12-16 14:26:06 +0100

    started to replace console log with async loggers

--- src/libs/crypto/cryptography.ts
+++ src/libs/crypto/cryptography.ts
@@ -14,6 +14,7 @@ import { promises as fs } from "fs"
 import forge from "node-forge"
 import { getSharedState } from "src/utilities/sharedState"
 import terminalkit from "terminal-kit"
+import log from "src/utilities/logger"
 
 import { forgeToHex } from "./forgeUtils"
 
