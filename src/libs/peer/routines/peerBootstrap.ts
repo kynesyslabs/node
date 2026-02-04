5d9e699d892172a992af4c2bf12bcbb49e7080c3
cwilvx <geoffreymungai45@gmail.com>
2026-02-04 17:06:47 +0300

    centralize marking peer as offline logic
    + prevent removing local node from own peerlist

--- src/libs/peer/routines/peerBootstrap.ts
+++ src/libs/peer/routines/peerBootstrap.ts
@@ -49,8 +49,8 @@ export default async function peerBootstrap(
             log.warning(
                 "[BOOTSTRAP] [FAILED] Failed to get peer identity: see above",
             )
-            peerManager.addOfflinePeer(blankPeer)
-            peerManager.removeOnlinePeer(blankPeer.identity)
+
+            PeerManager.markPeerOffline(blankPeer)
             continue
         }
 
